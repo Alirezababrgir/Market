f3b8e27f0ddc608b1878ad8eee4736b258454344
Alirezababrgir <zakhargithub021@gmail.com>
2024-12-04 10:35:59 -0500

    add theme for tailwind in tailwindconfig.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import "./App.css";
 import { Helmet } from "react-helmet-async";
 import MainLayout from "./components/mainlayout/mainlayout";
-import Header from "./components/header";
 function App() {
   return (
     <MainLayout>
       <Helmet title="مشتبا فروشگاه استیکر"></Helmet>
-      <div className="mx-auto max-w-6xl">
-        <Header />
-      </div>
     </MainLayout>
   );
 }
--- src/components/mainlayout/mainlayout.jsx
+++ src/components/mainlayout/mainlayout.jsx
@@ -1,9 +1,11 @@
+import Header from "../header";
 import Navbar from "../navbar";
 
 const MainLayout = ({ Children }) => {
   return (
     <div className="flex flex-col min-h-s">
       <Navbar />
+      <Header />
       <main>{Children}</main>
     </div>
   );
