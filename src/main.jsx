d278758fec2dea68c8e65c8d0fd36c65fc5cd8f3
Alirezababrgir <zakhargithub021@gmail.com>
2024-12-14 15:30:29 -0500

    create productForm,productDetails component

--- src/main.jsx
+++ src/main.jsx
@@ -9,25 +9,24 @@ import NotFound from "./components/NotFound.jsx";
 //import MainLayout from "./components/mainlayout/mainlayout.jsx";
 //import ProductCard from "./components/productcards.jsx";
 import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import ProductDetails from "./components/ProductDetails.jsx";
+import MainLayout from "./components/mainlayout/mainlayout.jsx";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App />, errorElement: <NotFound /> }/*,
+  { path: "/", element: <App />, errorElement: <NotFound /> },
   {
     path: "/products/:productID",
-    element: (
-      <MainLayout>
-        <ProductDetails />
-      </MainLayout>
-    ),
-  },
-  {
+    element: <ProductDetails />,
+  } 
+  ,/*{
     path: "/cart",
     element: (
       <MainLayout>
         <CardTable />
       </MainLayout>
     ),
-  },*/
+  }*/
 ]);
 
 createRoot(document.getElementById("root")).render(
