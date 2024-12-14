a043cd256ded2e020d52db8d554c4887143f5f2e
Alirezababrgir <zakhargithub021@gmail.com>
2024-12-14 11:23:05 -0500

    router for my project by react-outer-dom

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,38 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/index.js";
-import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import NotFound from "./components/NotFound.jsx";
+//import MainLayout from "./components/mainlayout/mainlayout.jsx";
+//import ProductCard from "./components/productcards.jsx";
+import { ToastContainer } from "react-toastify";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App />, errorElement: <NotFound /> }/*,
+  {
+    path: "/products/:productID",
+    element: (
+      <MainLayout>
+        <ProductDetails />
+      </MainLayout>
+    ),
+  },
+  {
+    path: "/cart",
+    element: (
+      <MainLayout>
+        <CardTable />
+      </MainLayout>
+    ),
+  },*/
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
+      <ToastContainer />
     </Provider>
   </StrictMode>
 );
